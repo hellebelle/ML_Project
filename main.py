f3910a0f664098ed65cf3eb2486e94de99c0a39f
Helana Maliakal <maliakaa@tcd.ie>
2020-12-06 20:20:05 +0000

    Completed testing stationarity and trend reduction

--- main.py
+++ main.py
@@ -33,12 +33,92 @@ df.iloc[:,0] = d
 #Use 'data' from this point forward
 data = df.drop([0],axis=1)
 data.index = df.iloc[:,0]
-print(data.head())
+
+# #Indexing techniques
+# #1. Specific the index as a string constant:
+#     data['2005-01']
+# #2. Specify the entire range:
+#     data['2005-01':'2005-05']
+# #3. Specify values for a whole year:
+#     data['2005']
 
 #TODO : Check for stationarity of multivariate time series (Statistical Test-Dickie Fuller) (Helen)
 #https://www.analyticsvidhya.com/blog/2018/09/multivariate-time-series-guide-forecasting-modeling-python-codes/#:~:text=A%20Multivariate%20time%20series%20has,used%20for%20forecasting%20future%20values.&text=In%20this%20case%2C%20there%20are,considered%20to%20optimally%20predict%20temperature.
 #https://www.analyticsvidhya.com/blog/2018/09/non-stationary-time-series-python/
 
+from statsmodels.tsa.stattools import adfuller
+def test_stationarity(timeseries, i):
+    #Determing rolling statistics
+    rolmean = pd.Series(timeseries).rolling(window=12).mean()
+    rolstd = pd.Series(timeseries).rolling(window=12).std()
+
+    #Plot rolling statistics:
+    orig = pyplot.plot(timeseries, color='blue',label='Original')
+    mean = pyplot.plot(rolmean, color='red', label='Rolling Mean')
+    std = pyplot.plot(rolstd, color='black', label = 'Rolling Std')
+    pyplot.legend(loc='best')
+    pyplot.title('Rolling Mean & Standard Deviationat for Column = {}:'.format(i))
+    pyplot.show()
+
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test for Column at {}:'.format(i))
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
+
+#check stationarity
+# for i in range(1,6):
+#     test_stationarity(data[i],i)
+
+#From testing of stationarity concluded that data[2:] are non-stationary
+def simple_trend_reduction(ts,i):
+    
+    #Take average of ‘k’ consecutive values depending on the frequency of time series. 
+    #Here we can take the average over the past 1 year, i.e. last 12 values
+    ts_log = np.log(ts)
+    #moving_avg = ts_log.rolling(12).mean()
+    # pyplot.plot(ts_log)
+    # pyplot.plot(moving_avg, color='red')
+    # pyplot.show()
+
+    # #Subtract rolling mean from original series
+    #ts_log_moving_avg_diff = ts_log - moving_avg
+    # print(ts_log_moving_avg_diff.head(12))
+
+    # #Check for stationarity
+    #ts_log_moving_avg_diff.dropna(inplace=True)
+    #test_stationarity(ts_log_moving_avg_diff,i)
+
+    #------Another Way which I think should be used------#
+    #We take a 'weighted moving average' where more recent values are given a higher weight
+    #Exponentially Weighted Moving Average(EWMA) - Assigning weights 
+    expwighted_avg = ts_log.ewm(halflife=12).mean()
+    # pyplot.plot(ts_log)
+    # pyplot.plot(expwighted_avg, color='red')
+
+    ts_log_ewma_diff = ts_log - expwighted_avg
+    # test_stationarity(ts_log_ewma_diff, i)
+    return ts_log_ewma_diff
+
+for i in range(2,6):
+    data[i] = simple_trend_reduction(data[i],i)
+
+#Simple trend reduction techniques discussed before don’t work in all cases, particularly the ones with high seasonality. 
+#For data[2] we need to try remove trend and seasonality
+def differencing(ts,i):
+    #We take the difference of the observation at a particular instant with that at the previous instant.
+    ts_log = np.log(ts)
+    ts_log_diff = ts_log - ts_log.shift()
+    # plt.plot(ts_log_diff)
+    ts_log_diff.dropna(inplace=True)
+    # test_stationarity(ts_log_diff,i)
+
+    return ts_log_diff
+
+data[2] = differencing(data[2],2)
+print(data)
 #TODO : Drop GDP and Consumer Price Index (or whichever we should after checking for stationarity) (Helen)
 
 #TODO : Normalise data (0 to 1) (Cian)
