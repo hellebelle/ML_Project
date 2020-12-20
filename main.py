acc44627785dcd1882e7b79e33bb6245e53c54ac
everlynpoh1108 <pohe@tcd.ie>
2020-12-20 14:42:35 +0000

    commit

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import datetime as dt
 from sklearn.preprocessing import Normalizer
 
 #load dataset
-df = pd.read_csv("./dataset.csv",comment='#',header=None,parse_dates=[0])
-dates = df.iloc[:,0]
+df = pd.read_csv("./dataset.csv",parse_dates=[0])
+dates = df['Month']
 dates = np.array(dates) + '01' #Concatenate with fixed day for all dates
 
 #Convert the time in first column from type string to type date.
@@ -31,10 +31,9 @@ def dataset_visualisation() :
 
 
 #Standard in time-series to use variable depicting date-time information as index
-df.iloc[:,0] = d
 #Use 'data' from this point forward
-data = df.drop([0],axis=1)
-data.index = df.iloc[:,0]
+data = df.drop(['Month'],axis=1)
+data.index = d
 # #Indexing techniques
 # #1. Specific the index as a string constant:
 #     data['2005-01']
@@ -48,77 +47,37 @@ data.index = df.iloc[:,0]
 #https://www.analyticsvidhya.com/blog/2018/09/non-stationary-time-series-python/
 
 from statsmodels.tsa.stattools import adfuller
-def test_stationarity(timeseries, i):
-    #Determing rolling statistics
-    rolmean = pd.Series(timeseries).rolling(window=12).mean()
-    rolstd = pd.Series(timeseries).rolling(window=12).std()
-
-    #Plot rolling statistics:
-    orig = pyplot.plot(timeseries, color='blue',label='Original')
-    mean = pyplot.plot(rolmean, color='red', label='Rolling Mean')
-    std = pyplot.plot(rolstd, color='black', label = 'Rolling Std')
-    pyplot.legend(loc='best')
-    pyplot.title('Rolling Mean & Standard Deviationat for Column = {}:'.format(i))
-    pyplot.show()
-
+def stationarity(timeseries,name):
     #Perform Dickey-Fuller test:
-    print('Results of Dickey-Fuller Test for Column at {}:'.format(i))
+    print('Results of Dickey-Fuller Test for :' + name)
     dftest = adfuller(timeseries, autolag='AIC')
     dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
     for key,value in dftest[4].items():
         dfoutput['Critical Value (%s)'%key] = value
     print(dfoutput)
 
-#check stationarity
-# for i in range(1,6):
-#     test_stationarity(data[i],i)
+#Check for stationarity for all features
+def stationarity_test() :
+    stationarity(data['Average Housing Price'],"Average Housing Price")   #Result : Stationary
+    stationarity(data['Mortgage Interest Rates'],"Mortgage Interest Rates") #Result : Non-stationary
+    stationarity(data['Consumer Price Index'],"Consumer Price Index") #Result : Stationary
+    stationarity(data['Yearly GDP Per Capita'],"Yearly GDP Per Capita") #Result: Non-stationary
+    stationarity(data['Net Yearly Household Income'],"Net Yearly Household Income") #Result : Non-stationary
 
-#From testing of stationarity concluded that data[2:] are non-stationary
-def simple_trend_reduction(ts,i):
-    
-    #Take average of ‘k’ consecutive values depending on the frequency of time series. 
-    #Here we can take the average over the past 1 year, i.e. last 12 values
-    ts_log = np.log(ts)
-    #moving_avg = ts_log.rolling(12).mean()
-    # pyplot.plot(ts_log)
-    # pyplot.plot(moving_avg, color='red')
-    # pyplot.show()
+#Differencing method on non-stationary data to convert them to stationary
+def differencing() :
+    data['diffMIR'] = data['Mortgage Interest Rates']-data['Mortgage Interest Rates'].shift(1)
+    data['diffGDP'] = data['Yearly GDP Per Capita']-data['Yearly GDP Per Capita'].shift(1)
+    data['diffYHI'] = data['Net Yearly Household Income']-data['Net Yearly Household Income'].shift(1)
 
-    # #Subtract rolling mean from original series
-    #ts_log_moving_avg_diff = ts_log - moving_avg
-    # print(ts_log_moving_avg_diff.head(12))
+#Check for stationarity after differencing
+def diff_stationarity_test() :
+    differencing()
+    stationarity(data['diffMIR'],"Differenced Mortgage Interest Rates")
+    stationarity(data['diffGDP'],"Differenced Yearly GDP Per Capita")
+    stationarity(data['diffYHI'],"Differenced Net Yearly Household Income")
 
-    # #Check for stationarity
-    #ts_log_moving_avg_diff.dropna(inplace=True)
-    #test_stationarity(ts_log_moving_avg_diff,i)
-
-    #------Another Way which I think should be used------#
-    #We take a 'weighted moving average' where more recent values are given a higher weight
-    #Exponentially Weighted Moving Average(EWMA) - Assigning weights 
-    expwighted_avg = ts_log.ewm(halflife=12).mean()
-    # pyplot.plot(ts_log)
-    # pyplot.plot(expwighted_avg, color='red')
-
-    ts_log_ewma_diff = ts_log - expwighted_avg
-    # test_stationarity(ts_log_ewma_diff, i)
-    return ts_log_ewma_diff
-
-for i in range(2,6):
-    data[i] = simple_trend_reduction(data[i],i)
-
-#Simple trend reduction techniques discussed before don’t work in all cases, particularly the ones with high seasonality. 
-#For data[2] we need to try remove trend and seasonality
-def differencing(ts,i):
-    #We take the difference of the observation at a particular instant with that at the previous instant.
-    ts_diff = ts - ts.shift(1)
-    # plt.plot(ts_log_diff)
-    ts_diff.dropna(inplace=True)
-    test_stationarity((ts_diff),i)
-
-    return ts_diff
-
-data[2] = differencing(data[2],2)
-print(data)
+diff_stationarity_test()
 
 def normalizeDataframe(dataFrame):
     df_num = dataFrame.select_dtypes(include=[np.number])
